73949d163b4d94d575a775c119e2ae095cf3958c
indemnity83 <kklaus@indemnity83.com>
2016-03-30 07:47:10 -0700

    Fix an issue with trying to rename multiple columns in on transaction on sqlite

--- database/migrations/2016_03_27_040504_normalize_schema_for_laravel5.php
+++ database/migrations/2016_03_27_040504_normalize_schema_for_laravel5.php
@@ -12,20 +12,24 @@ class NormalizeSchemaForLaravel5 extends Migration
      */
     public function up()
     {
-        Schema::table('builds', function($table) {
-			$table->renameColumn('is_published', 'published');
+        Schema::table('builds', function ($table) {
+            $table->renameColumn('is_published', 'published');
+        });
+        Schema::table('builds', function ($table) {
             $table->dropColumn('forge');
-		});
+        });
 
-        Schema::table('mods', function($table) {
-			$table->renameColumn('name', 'slug');
+        Schema::table('mods', function ($table) {
+            $table->renameColumn('name', 'slug');
+        });
+
+        Schema::table('mods', function ($table) {
             $table->renameColumn('pretty_name', 'name');
-		});
+        });
 
-        Schema::table('users', function($table) {
-			$table->renameColumn('username', 'name');
-		});
-
+        Schema::table('users', function ($table) {
+            $table->renameColumn('username', 'name');
+        });
     }
 
     /**
@@ -35,18 +39,24 @@ class NormalizeSchemaForLaravel5 extends Migration
      */
     public function down()
     {
-        Schema::table('builds', function($table) {
-			$table->renameColumn('published', 'is_published');
-            $table->string('forge')->nullable();
-		});
+        Schema::table('builds', function ($table) {
+            $table->renameColumn('published', 'is_published');
+        });
 
-        Schema::table('mods', function($table) {
+        Schema::table('builds', function ($table) {
+            $table->string('forge')->nullable();
+        });
+
+        Schema::table('mods', function ($table) {
             $table->renameColumn('name', 'pretty_name');
+        });
+
+        Schema::table('mods', function ($table) {
             $table->renameColumn('slug', 'name');
         });
 
-        Schema::table('users', function($table) {
-			$table->renameColumn('name', 'username');
-		});
+        Schema::table('users', function ($table) {
+            $table->renameColumn('name', 'username');
+        });
     }
 }
