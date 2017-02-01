65905d71d131726ade2888921f892f42cf561878
Kyle Klaus <kklaus@indemnity83.com>
2017-01-31 16:49:47 -0800

    Shift broadcast service provider
    Laravel 5.4 moved the broadcast channels configuration into a stub file under routes

---
+++ routes/channels.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of solder.
+ *
+ * (c) Kyle Klaus <kklaus@indemnity83.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Broadcast Channels
+|--------------------------------------------------------------------------
+|
+| Here you may register all of the event broadcasting channels that your
+| application supports. The given channel authorization callbacks are
+| used to check if an authenticated user can listen to the channel.
+|
+*/
+Broadcast::channel('App.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
