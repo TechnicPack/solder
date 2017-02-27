d0ffb45c843d9390f4e153ffa94278ccce253cb7
Kyle Klaus <kklaus@indemnity83.com>
2017-02-26 21:20:22 -0800

    Correct branding in page title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Technic Solder</title>
+    <title>Solder</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
