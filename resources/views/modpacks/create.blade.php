ef9e9cfdc787af1eb96c93953d10df89f56440f6
Lukas Heddendorp <lu.heddendorp@gmail.com>
2017-03-02 16:33:16 +0100

    Update modpack forms to use fewer rows

--- resources/views/modpacks/create.blade.php
+++ resources/views/modpacks/create.blade.php
@@ -49,77 +49,88 @@
         <form method="post" action="{{ route('modpacks.store') }}">
             {{ csrf_field() }}
 
-            {{-- name --}}
-            <label class="label">Name</label>
-            <p class="control">
-                <input class="input" type="text" name="name" value="{{ old('name') }}">
-            </p>
-
-            {{-- slug --}}
-            <label class="label">Slug</label>
-            <p class="control">
-                <input class="input" type="text" name="slug" value="{{ old('slug') }}">
-            </p>
-
-            {{-- description --}}
-            <label class="label">Description</label>
-            <p class="control">
-                <input class="input" type="text" name="description" value="{{ old('description') }}">
-            </p>
-
-            {{-- privacy --}}
-            <label class="label">Privacy</label>
-            <p class="control">
-                    <span class="select">
-                        <select class="input" name="privacy">
-                            <option value="public">Public</option>
-                            <option value="unlisted">Unlisted</option>
-                            <option value="private">Private</option>
-                        </select>
-                    </span>
-            </p>
-
-            {{-- tags --}}
-            <label class="label">Tags</label>
-            <p class="control">
-                <input class="input is-disabled" type="text" name="tags[]" placeholder="Not Yet Implemented">
-            </p>
-
-            {{-- icon --}}
-            <label class="label">Icon</label>
-            <p class="control">
-                <input type="file" name="icon" accept="image/*">
-            </p>
-
-            {{-- logo --}}
-            <label class="label">Logo</label>
-            <p class="control">
-                <input type="file" name="logo" accept="image/*">
-            </p>
-
-            {{-- background --}}
-            <label class="label">Background</label>
-            <p class="control">
-                <input type="file" name="background" accept="image/*">
-            </p>
-
-            {{-- overview --}}
-            <label class="label">Overview</label>
-            <p class="control">
-                <textarea class="input" name="overview" rows="5">{{ old('overview') }}</textarea>
-            </p>
-
-            {{-- help --}}
-            <label class="label">Help</label>
-            <p class="control">
-                <textarea class="input" name="help" rows="5">{{ old('help') }}</textarea>
-            </p>
-
-            {{-- license --}}
-            <label class="label">License</label>
-            <p class="control">
-                <textarea class="input" name="license" rows="5">{{ old('license') }}</textarea>
-            </p>
+            <div class="columns is-multiline">
+                <div class="column is-half">
+                    <label class="label">Name</label>
+                    <div class="control">
+                        <input class="input" type="text" name="name">
+                    </div>
+                </div>
+                <div class="column is-half">
+                    <label class="label">Slug</label>
+                    <div class="control">
+                        <input class="input is-expanded" type="text" name="slug"
+                        >
+                    </div>
+                </div>
+                <div class="column is-12">
+                    <label class="label">Description</label>
+                    <div class="control">
+                        <input class="input" type="text" name="description"
+                        >
+                    </div>
+                </div>
+                <div class="column is-quarter">
+                    <label class="label">Privacy</label>
+                    <div class="control">
+                        <div class="select is-fullwidth">
+                            <select name="privacy">
+                                <option value="public">Public
+                                </option>
+                                <option value="unlisted">
+                                    Unlisted
+                                </option>
+                                <option value="private">Private
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="column is-quarter">
+                    <label class="label">Icon</label>
+                    <div class="control">
+                        <input type="file" name="icon" accept="image/*">
+                    </div>
+                </div>
+                <div class="column is-quarter">
+                    <label class="label">Logo</label>
+                    <div class="control">
+                        <input type="file" name="logo" accept="image/*">
+                    </div>
+                </div>
+                <div class="column is-quarter">
+                    <label class="label">Background</label>
+                    <div class="control">
+                        <input type="file" name="background" accept="image/*">
+                    </div>
+                </div>
+                <div class="column is-12">
+                    <label class="label">Tags</label>
+                    <div class="control">
+                        <input class="input is-disabled" type="text" name="tags[]" placeholder="Not Yet Implemented">
+                    </div>
+                </div>
+                <div class="column one-third">
+                    <label class="label">Overview</label>
+                    <div class="control">
+                        <textarea class="textarea" name="overview"
+                                  rows="5"></textarea>
+                    </div>
+                </div>
+                <div class="column one-third">
+                    <label class="label">Help</label>
+                    <div class="control">
+                        <textarea class="textarea" name="help" rows="5"></textarea>
+                    </div>
+                </div>
+                <div class="column one-third">
+                    <label class="label">License</label>
+                    <div class="control">
+                        <textarea class="textarea" name="license"
+                                  rows="5"></textarea>
+                    </div>
+                </div>
+            </div>
 
             {{-- Submit --}}
             <div class="control is-grouped">
--- resources/views/modpacks/edit.blade.php
+++ resources/views/modpacks/edit.blade.php
@@ -21,7 +21,7 @@
                 <ul>
                     <li><a href="{{ route('modpacks.index') }}">Overview</a></li>
                     <li><a href="{{ route('modpacks.create') }}">Create new</a></li>
-                    <li class="is-active"><a>Edit modpack</a></li>
+                    <li class="is-active"><a>Edit {{ $modpack->name }}</a></li>
                     <li><a href="{{ route('modpacks.show', $modpack->id) }}">Show {{ $modpack->name }}</a></li>
                 </ul>
             </div>
@@ -51,78 +51,88 @@
             {{ csrf_field() }}
             {{ method_field('patch') }}
 
-            {{-- name --}}
-            <label class="label">Name</label>
-            <p class="control">
-                <input class="input is-expanded" type="text" name="name" value="{{ old('name', $modpack->name) }}">
-            </p>
-
-            {{-- slug --}}
-            <label class="label">Slug</label>
-            <p class="control">
-                <input class="input is-expanded" type="text" name="slug" value="{{ old('slug', $modpack->slug) }}">
-            </p>
-
-            {{-- description --}}
-            <label class="label">Description</label>
-            <p class="control">
-                <input class="input" type="text" name="description"
-                       value="{{ old('description', $modpack->description) }}">
-            </p>
-
-            {{-- privacy --}}
-            <label class="label">Privacy</label>
-            <p class="control">
-                    <span class="select">
-                        <select class="input" name="privacy">
-                            <option value="public" {{ $modpack->privacy == 'public' ? 'selected' : '' }}>Public</option>
-                            <option value="unlisted" {{ $modpack->privacy == 'unlisted' ? 'selected' : '' }}>Unlisted</option>
-                            <option value="private" {{ $modpack->privacy == 'private' ? 'selected' : '' }}>Private</option>
-                        </select>
-                    </span>
-            </p>
-
-            {{-- tags --}}
-            <label class="label">Tags</label>
-            <p class="control">
-                <input class="input is-disabled" type="text" name="tags[]" placeholder="Not Yet Implemented">
-            </p>
-
-            {{-- icon --}}
-            <label class="label">Icon</label>
-            <p class="control">
-                <input type="file" name="icon" accept="image/*">
-            </p>
-
-            {{-- logo --}}
-            <label class="label">Logo</label>
-            <p class="control">
-                <input type="file" name="logo" accept="image/*">
-            </p>
-
-            {{-- background --}}
-            <label class="label">Background</label>
-            <p class="control">
-                <input type="file" name="background" accept="image/*">
-            </p>
-
-            {{-- overview --}}
-            <label class="label">Overview</label>
-            <p class="control">
-                <textarea class="input" name="overview" rows="5">{{ old('overview', $modpack->overview) }}</textarea>
-            </p>
-
-            {{-- help --}}
-            <label class="label">Help</label>
-            <p class="control">
-                <textarea class="input" name="help" rows="5">{{ old('help', $modpack->help) }}</textarea>
-            </p>
-
-            {{-- license --}}
-            <label class="label">License</label>
-            <p class="control">
-                <textarea class="input" name="license" rows="5">{{ old('license', $modpack->license) }}</textarea>
-            </p>
+            <div class="columns is-multiline">
+                <div class="column is-half">
+                    <label class="label">Name</label>
+                    <div class="control">
+                        <input class="input" type="text" name="name" value="{{ old('name', $modpack->name) }}">
+                    </div>
+                </div>
+                <div class="column is-half">
+                    <label class="label">Slug</label>
+                    <div class="control">
+                        <input class="input is-expanded" type="text" name="slug"
+                               value="{{ old('slug', $modpack->slug) }}">
+                    </div>
+                </div>
+                <div class="column is-12">
+                    <label class="label">Description</label>
+                    <div class="control">
+                        <input class="input" type="text" name="description"
+                               value="{{ old('description', $modpack->description) }}">
+                    </div>
+                </div>
+                <div class="column is-quarter">
+                    <label class="label">Privacy</label>
+                    <div class="control">
+                        <div class="select is-fullwidth">
+                            <select name="privacy">
+                                <option value="public" {{ $modpack->privacy == 'public' ? 'selected' : '' }}>Public
+                                </option>
+                                <option value="unlisted" {{ $modpack->privacy == 'unlisted' ? 'selected' : '' }}>
+                                    Unlisted
+                                </option>
+                                <option value="private" {{ $modpack->privacy == 'private' ? 'selected' : '' }}>Private
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="column is-quarter">
+                    <label class="label">Icon</label>
+                    <div class="control">
+                        <input type="file" name="icon" accept="image/*">
+                    </div>
+                </div>
+                <div class="column is-quarter">
+                    <label class="label">Logo</label>
+                    <div class="control">
+                        <input type="file" name="logo" accept="image/*">
+                    </div>
+                </div>
+                <div class="column is-quarter">
+                    <label class="label">Background</label>
+                    <div class="control">
+                        <input type="file" name="background" accept="image/*">
+                    </div>
+                </div>
+                <div class="column is-12">
+                    <label class="label">Tags</label>
+                    <div class="control">
+                        <input class="input is-disabled" type="text" name="tags[]" placeholder="Not Yet Implemented">
+                    </div>
+                </div>
+                <div class="column one-third">
+                    <label class="label">Overview</label>
+                    <div class="control">
+                        <textarea class="textarea" name="overview"
+                                  rows="5">{{ old('overview', $modpack->overview) }}</textarea>
+                    </div>
+                </div>
+                <div class="column one-third">
+                    <label class="label">Help</label>
+                    <div class="control">
+                        <textarea class="textarea" name="help" rows="5">{{ old('help', $modpack->help) }}</textarea>
+                    </div>
+                </div>
+                <div class="column one-third">
+                    <label class="label">License</label>
+                    <div class="control">
+                        <textarea class="textarea" name="license"
+                                  rows="5">{{ old('license', $modpack->license) }}</textarea>
+                    </div>
+                </div>
+            </div>
 
             {{-- Submit --}}
             <div class="control is-grouped">
