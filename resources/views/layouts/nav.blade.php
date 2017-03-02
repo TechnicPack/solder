c2d316089445e73b43f10f9f453227576bf77af7
Lukas Heddendorp <lu.heddendorp@gmail.com>
2017-03-02 09:04:49 +0100

    Begin layout improvements

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,51 +1,53 @@
-<nav class="nav has-shadow" id="top">
-  <div class="container">
-    <div class="nav-left">
-      <a class="nav-item" href="/">
-          <img src="/img/title.png" alt="Solder" />
-          <span class="is-hidden-mobile">{{ $appVersion or null }}</span>
-      </a>
-    </div>
-    <span class="nav-toggle" onclick="toggleNav()">
-      <span></span>
-      <span></span>
-      <span></span>
-    </span>
-    <div id="nav-menu" class="nav-right nav-menu">
-      <a href="{{ route('dashboard') }}" class="nav-item is-tab">
-        Dashboard
-      </a>
-      <a href="{{ route('modpacks.index') }}" class="nav-item is-tab">
-        Modpacks
-      </a>
-      <a href="#" class="nav-item is-tab">
-        Resources
-      </a>
-      <a href="#" class="nav-item is-tab">
-        Settings
-      </a>
-      @if (Auth::guest())
-          <span class="nav-item">
-            <a class="button" href="{{ route('login') }}">
+<div class="hero-head">
+    <header class="nav">
+        <div class="container">
+            <div class="nav-left">
+                <a class="nav-item">
+                    <img src="/img/logo.svg" alt="Logo">
+                    <span class="title">solder</span>
+                </a>
+            </div>
+            <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+            <div class="nav-right nav-menu">
+                <a href="{{ route('dashboard') }}" class="nav-item is-tab">
+                    Dashboard
+                </a>
+                <a href="{{ route('modpacks.index') }}" class="nav-item is-tab">
+                    Modpacks
+                </a>
+                <a href="#" class="nav-item is-tab">
+                    Resources
+                </a>
+                <a href="#" class="nav-item is-tab">
+                    Settings
+                </a>
+                @if (Auth::guest())
+                    <span class="nav-item">
+            <a class="button is-inverted" href="{{ route('login') }}">
               Log in
             </a>
-            <a class="button is-info" href="{{ route('register') }}">
+            <a class="button is-info is-inverted" href="{{ route('register') }}">
               Sign up
             </a>
           </span>
-      @else
-          <span class="nav-item" href="{{ route('login') }}">
-              <a class="button" href="{{ route('logout') }}"
-                  onclick="event.preventDefault();
+                @else
+                    <span class="nav-item" href="{{ route('login') }}">
+              <a class="button is-inverted is-primary" href="{{ route('logout') }}"
+                 onclick="event.preventDefault();
                            document.getElementById('logout-form').submit();">
-                  Logout
+                  <span>Logout</span>
               </a>
 
               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                   {{ csrf_field() }}
               </form>
           </span>
-      @endif
-    </div>
-  </div>
-</nav>
+                @endif
+            </div>
+        </div>
+    </header>
+</div>
