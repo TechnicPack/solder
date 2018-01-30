0dbfe181794611874eca39447701ac8b289af53c
Kyle Klaus <kklaus@indemnity83.com>
2018-01-29 16:12:23 -0800

    revert: ci(packages): require league/flysystem 1.0.8
    This reverts commit dabc1f98b1c5b942303d4f9079cce2957f7204bb.

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.32 on 2018-01-29.
+ * Generated for Laravel 5.5.32 on 2018-01-30.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
