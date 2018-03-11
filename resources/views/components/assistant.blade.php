db6be015f557b7f932b586e222bf4e1b50bb3d20
Noles <noles@a3k.net>
2018-03-11 21:08:15 +0100

    Allow to close assistant/help and remember the option

--- resources/views/components/assistant.blade.php
+++ resources/views/components/assistant.blade.php
@@ -1,17 +1,7 @@
 @assistant
-<section class="hero is-info">
-    <div class="notification is-info is-radiusless">
-        <a href="/settings/general" class="delete"></a>
-        <nav class="columns">
-            <div class="column is-narrow">
-                <figure class="image is-64x64 is-pulled-left">
-                    <img src="/img/book.png" />
-                </figure>
-            </div>
-            <div class="column">
-                {{ $slot }}
-            </div>
-        </nav>
-    </div>
+<section class="section is-paddingless">
+    <assistant type="info">
+        {{ $slot }}
+    </assistant>
 </section>
 @endassistant
