0041c4265de274a2433bb77c4451d4f83e6285b3
Kyle Klaus <kklaus@indemnity83.com>
2018-03-12 11:56:46 -0700

    Remove commented out code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,8 +33,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        //
     }
 
     /**
