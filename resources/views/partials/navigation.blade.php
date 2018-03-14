b1601f0bb208a88c04ca54a9cfdbd12b554b4233
Kyle Klaus <kklaus@indemnity83.com>
2018-03-14 11:13:32 -0700

    Disable OAuth Functionality (#101)
    still keeping laravel passport for jwt, and easy management of personal access tokens
    
    closes #100
    
    * remove duplicate settings link from navigation

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -5,11 +5,10 @@
     <div class="navbar-menu">
         <div class="navbar-end">
             <a class="navbar-item" href="/library"> Library </a>
-            <a class="navbar-item" href="/settings/about"> Settings </a>
+            <a class="navbar-item" href="/settings/api"> Settings </a>
             <div class="navbar-item has-dropdown is-hoverable">
                 <a class="navbar-link"> {{ Auth()->user()->username }} </a>
                 <div class="navbar-dropdown is-right is-radiusless">
-                    <a class="navbar-item" href="/profile/tokens"> Account Settings </a>
                     <a class="navbar-item" href="javascript:{}" onclick="document.getElementById('logout').submit(); return false;"> Log Out </a>
 
                     <form id="logout" action="{{ route('auth.logout') }}" method="POST">
---
+++ routes/oauth.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of Solder.
+ *
+ * (c) Kyle Klaus <kklaus@indemnity83.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+Route::get('scopes', '\Laravel\Passport\Http\Controllers\ScopeController@all');
+Route::get('personal-access-tokens', '\Laravel\Passport\Http\Controllers\PersonalAccessTokenController@forUser');
+Route::post('personal-access-tokens', '\Laravel\Passport\Http\Controllers\PersonalAccessTokenController@store');
+Route::delete('personal-access-tokens/{token_id}', '\Laravel\Passport\Http\Controllers\PersonalAccessTokenController@destroy');
+
+// Transient Access Token Routs ...
+Route::post('token/refresh', '\Laravel\Passport\Http\Controllers\TransientTokenController@refresh');
