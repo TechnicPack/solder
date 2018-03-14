d61f3438b73aacf2eeb6617bd622a744258156eb
Kyle Klaus <kklaus@indemnity83.com>
2018-03-13 18:06:06 -0700

    remove duplicate settings link from navigation

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -9,7 +9,6 @@
             <div class="navbar-item has-dropdown is-hoverable">
                 <a class="navbar-link"> {{ Auth()->user()->username }} </a>
                 <div class="navbar-dropdown is-right is-radiusless">
-                    <a class="navbar-item" href="/profile/tokens"> Account Settings </a>
                     <a class="navbar-item" href="javascript:{}" onclick="document.getElementById('logout').submit(); return false;"> Log Out </a>
 
                     <form id="logout" action="{{ route('auth.logout') }}" method="POST">
