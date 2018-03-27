1d1ab5c0cfe44be1ad6f0aa008f2745c22d463be
Siriuo <siriuo@outlook.com>
2018-03-27 20:52:16 +0100

    Tidied up file uploading. Added ability to upload a zip without double zipping :) Usefull for people who want to upload zip configs or pre-packaged zip files

--- app/Http/Controllers/PackageReleasesController.php
+++ app/Http/Controllers/PackageReleasesController.php
@@ -41,37 +41,32 @@ class PackageReleasesController extends Controller
         request()->validate([
             'version' => ['required', 'regex:/^[a-zA-Z0-9_-][.a-zA-Z0-9_-]*$/', Rule::unique('releases')->where('package_id', $package->id)],
             'type' => ['required'],
-            'file' => ['required'],
+            'file' => ['required', 'min:1', 'mimes:zip,jar,cfg,json'],
         ]);
-
         $file_name = $_FILES['file']['name'];
-
-        $file = $request->file('file')->store('tmp');
-        $archive = new ZipArchive();
-        $archive_directory = "modpack/" . $package->slug;
-        $public = storage_path('app/public/');
-        $archive_name = $archive_directory . "/" . "{$package->slug}-{$request->version}.zip";
-        $hash_path = url('/') ."/" . "storage/" . "{$package->slug}/" . "{$package->slug}-{$request->version}.zip";
-        $storage_path = "{$package->slug}-{$request->version}.zip";
-
-
-
-        if(!Storage::exists($archive_directory)) {
-            Storage::makeDirectory($archive_directory);
+        $file_info = pathinfo($file_name);
+        $tmp_file = $request->file('file')->store('tmp');
+        $package_name = "{$package->slug}-{$request->version}.zip";
+        if(!Storage::exists("modpack/{$package->slug}")) {
+            Storage::makeDirectory("modpack/{$package->slug}");
         }
+        if($file_info['extension'] == 'zip'){
+            Storage::move($tmp_file, "modpack/{$package->slug}/" . $package_name);
+        }else{
+            $archive = new ZipArchive();
+            $archive_path = storage_path("app/public/modpack/{$package->slug}");
+            if($archive->open($archive_path . "/" . $package_name, ZipArchive::CREATE) === TRUE){
+                $archive->addFile(storage_path("app/public/" . $tmp_file), request()->input('type') . "/". $file_name);
+            }
+            $archive->close();
 
-        if($archive->open($public . "/" . $archive_name, ZipArchive::CREATE) === TRUE){
-            $archive->addFile(storage_path("app/public/" . $file), request()->input('type') . "/". $file_name);
         }
-
-        $archive->close();
-
-        Storage::disk('public')->delete($file);
-
+        Storage::disk('public')->delete($tmp_file);
+        $hash_path = url('/') ."/" . "storage/" . "{$package->slug}/" . $package_name;
         Release::create([
             'package_id' => $package->id,
             'version' => $request->version,
-            'path' => $storage_path,
+            'path' => $package_name,
             'md5' => FileHash::hash($hash_path),
             'filesize' => request()->file('file')->getSize(),
         ]);
