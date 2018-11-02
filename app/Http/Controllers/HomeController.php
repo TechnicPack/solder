1849b9d81ad64b941c8b79cae530794c47335f08
Kyle Klaus <kklaus@indemnity83.com>
2018-11-01 22:25:03 -0700

    scaffold basic login and registration views and routes

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of TechnicPack Solder.
+ *
+ * (c) Syndicate LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
