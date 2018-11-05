7de49e9821efbc4faa3b0cd017e1f35144fd35f0
Kyle Klaus <kklaus@indemnity83.com>
2018-11-05 14:42:31 -0800

    create initial modpack vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,10 +15,11 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
 Vue.component('passport-clients', require('./components/passport/Clients.vue'));
 Vue.component('passport-authorized-clients', require('./components/passport/AuthorizedClients.vue'));
 Vue.component('passport-personal-access-tokens', require('./components/passport/PersonalAccessTokens.vue'));
+Vue.component('solder-modpacks', require('./components/solder/Modpacks.vue'));
+Vue.prototype.$hostname = '/api';
 
 const app = new Vue({
     el: '#app'
---
+++ resources/js/components/solder/Modpacks.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="mb-4">
+        <div class="card card-default">
+            <div class="card-header card-header-light d-flex align-items-center justify-content-between">
+                <h3 class="mb-0">Modpacks</h3>
+                <button type="button" class="btn btn-primary">Create Modpack</button>
+            </div>
+
+            <table class="table table-hover mb-0">
+                <thead class="thead-light">
+                <tr>
+                    <th>Name</th>
+                    <th>Slug</th>
+                    <th>&nbsp;</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="modpack in modpacks" :key="modpack.id">
+                    <th scope="row" class="align-middle">
+                        <img :src="modpack.icon" class="img-thumbnail" width="50px" height="50px"/>
+                        {{ modpack.name }}
+                    </th>
+                    <td class="align-middle">
+                        {{ modpack.slug }}
+                    </td>
+                    <td class="text-right align-middle">
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" class="btn btn-link">View</button>
+                            <button type="button" class="btn btn-link">Edit</button>
+                            <button type="button" class="btn btn-link">Delete</button>
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        /*
+         * The component's data.
+         */
+        data() {
+            return {
+                modpacks: []
+            }
+        },
+
+
+        /**
+         * Prepare the component (Vue 2.x).
+         */
+        mounted() {
+            this.prepareComponent();
+        },
+
+
+        methods: {
+            /**
+             * Prepare the component.
+             */
+            prepareComponent() {
+                this.getModpacks();
+            },
+
+            /**
+             * Get all of the OAuth clients for the user.
+             */
+            getModpacks() {
+                axios.get(`${this.$hostname}/modpacks`)
+                    .then(response => {
+                        this.modpacks = response.data;
+                    });
+            },
+        }
+    }
+</script>
